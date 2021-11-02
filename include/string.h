c4b37e0502fb6f640e943da7ba56ef7c4725eaca
Ariel Sokolsky <7325708-arielsokolsky@users.noreply.gitlab.com>
2021-11-02 16:30:26 +0000

    Feature/string

---
+++ include/string.h
@@ -0,0 +1,14 @@
+#ifndef STRING_H
+#define STRING_H
+#include "./types.h"
+
+//return the length of a string
+int strlen(string str);
+
+//return if string the same
+bool strcmp(string str1, string str2);
+
+//reverse the string
+string strrev(string str);
+
+#endif
