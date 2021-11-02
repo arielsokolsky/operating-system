e96a753ccf711578e7328046a5baccfd3340444a
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-11-02 12:25:14 -0400

    add strcmp

--- src/string.c
+++ src/string.c
@@ -11,3 +11,26 @@ int strlen(string str)
     while(str[i++]);  
     return i-1;
 }
+
+/*
+the fuction get 2 string and return if equel
+param: 2 strings
+return: if string equel
+*/
+bool strcmp(string str1, string str2)
+{
+    bool equel = true;
+
+    //check if length equel
+    if(strlen(str1) != strlen(str2))
+        equel = false;
+
+    //check that string the same
+    for(int i = 0; i < strlen(str1); i++)
+    {
+        if(str1[i] != str2[i])
+            equel = false;
+    } 
+
+    return equel;
+}
