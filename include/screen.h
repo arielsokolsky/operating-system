08374c2a7d3afa8ab96adfad7cd691ad90741876
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-11-02 12:44:08 -0400

    create screen.h

---
+++ include/screen.h
@@ -0,0 +1,18 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include "string.h"
+
+#define VIDEO_ADDRESS 0xb8000
+#define NUM_OF_LINES 25
+#define NUM_OF_RAWS 80
+
+enum COLORS{Black = 0, Blue, Green, Cyan, Red, Purple, Brown, Gray, Dark_gray, Light_blue, light_green, Light_cyan, Light_Red, light_purple, Yellow , White};
+static int currentColor = White;
+
+void printch(char ch);
+void print(string str);
+void changeTextColor(int);
+void clearScreen();
+
+#endif
