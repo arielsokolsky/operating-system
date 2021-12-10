2d8c988d78925c13b2c83eefb5f96c6b04562e17
Yotam shalit <yotam.shalit@gmail.com>
2021-12-10 18:11:31 +0200

    added assembly paging

--- include/paging.h
+++ include/paging.h
@@ -4,21 +4,23 @@
 
 struct page {
     //checks if the file exists
-    unsigned_int8 present : 1; 
+    _Bool present; 
     // if the bit is clear then the file is read only, if the bit is one then it read and write
-    unsigned_int8 readwrite : 1;
+    _Bool readwrite;
     // says the file supervision level
-    unsigned_int8 user : 1;
+    _Bool user;
     // Has the page been accessed since the last os refresh?
-    unsigned_int8 lastAccessed : 1; 
+    _Bool lastAccessed; 
     //Has the page been written into since the last user os refresh?
-    unsigned_int8 lastWritten : 1;   
+    _Bool lastWritten;   
     // unused bits and flags
     unsigned_int8 unusedBits : 7;   
     // Frame address
     unsigned_int32 frameAddress : 20;  
 };
 
+extern void enablePaging();
+extern void loadPageDirectory(unsigned int*);
 
 struct page_table
 {
@@ -34,5 +36,5 @@ struct page_directory
     unsigned_int32 physicalAddress;
 };
 
-struct page *get_page(uint32_t address, int makeNew, struct page_directory *dir);
+struct page *get_page(unsigned_int32 address, int makeNew, struct page_directory *dir);
 #endif
--- src/paging.c
+++ src/paging.c
@@ -1,16 +1,29 @@
 #include "../include/paging.h"
 
+struct page_directory *kernel_directory;
+struct page_directory *current_directory;
 
 
+void initialize_paging() {
 
+    current_directory = kernel_directory;
+    
+    // Go ahead and allocate all the page tables for the kernel.
+    // This is wasteful, but a lot easier than figuring out how to build
+    // a kernel page allocator.
+    unsigned_int32 i = 0;
+    enablePaging();
+    loadPageDirectory(kernel_directory);
 
-struct page *get_page(uint32_t address, int makeNew, struct page_directory *dir)
+}
+
+struct page *get_page(unsigned_int32 address, int makeNew, struct page_directory *dir)
 {
-    // removes the files checksum from the address by shifting the address using division of the necessery amount of bits
+    // removes the files offset from the address by shifting the address using division of the necessery amount of bits
     address /= 0x1000;
     // Find the page table containing this address
     unsigned_int32 table_idx = address / 1024;
-    if (dir->tables[table_idx] != NULL) // If this table is already assigned in the table
+    if (dir->tables[table_idx] != 0) // If this table is already assigned in the table
     {
         //returns the page in the correct table and page
         return &dir->tables[table_idx]->pages[address%1024];
@@ -21,6 +34,6 @@ struct page *get_page(uint32_t address, int makeNew, struct page_directory *dir)
     }
     else
     {
-        return NULL;
+        return 0;
     }
 }
