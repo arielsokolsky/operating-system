4dc2a65a09c0d812d66350288011e51fc1e43d42
arielsokolsky <arielsoko123@gmail.com>
2022-01-03 17:08:47 +0200

    move yotam shalit changes branch

--- asm_src/tss_asm.asm
+++ asm_src/tss_asm.asm
@@ -1,7 +1,28 @@
-global flush_tss
+global tss_flash
+global asm_switch
+
+tss_flush:
+    mov ax, 0x2B
+    lrt ax
+    ret
 
 
-flush_tss:
-    mov ax, 0x2c
-    ltr ax
+
+asm_switch:
+    cli
+    mov ax 0x23
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov eax, esp
+    pushl 0x23
+    pushl eax
+    pushf
+    pop eax
+    or eax, 0x200
+    push eax
+
+    pushl 0x1B
     ret
--- c_src/tss.c
+++ c_src/tss.c
@@ -1,5 +1,6 @@
 #include "../include/tss.h"
-tss_t myTss;
+#include "../include/gdt.h"
+tss_t system_tss;
 
 
 /*
@@ -9,7 +10,27 @@ return: none
 */
 void tssSetStack(uint32 stack)
 {
-    myTss.stack_segment0.esp = stack;
+    system_tss.stack_segment0.esp = stack;
 }
 
+
+void tss_install(uint32 index, uint16 ss, uint16 esp)
+{
+    uint32 tss_addr = (uint32)&system_tss;
+    uint32 size = tss_addr + sizeof(tss_t);
+
+    set_gdt_gate(index, tss_addr, size, 0xE9, 0);
+
+    system_tss.stack_segment0.ss = ss;
+    system_tss.stack_segment0.esp = esp;
+
+    system_tss.segment_registers.cs = 0x0B;
+    system_tss.segment_registers.ds = 0x13;
+    system_tss.segment_registers.es = 0x13;
+    system_tss.segment_registers.fs = 0x13;
+    system_tss.segment_registers.gs = 0x13;
+    system_tss.segment_registers.ss = 0x13;
+
+    tss_flush();
+}
 
